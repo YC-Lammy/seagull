66b19cc29b5bf94ee314d8df59d1855489c3d65c
YC-L <76801636+YC-Lammy@users.noreply.github.com>
2021-10-17 21:45:09 +0100

    main

--- main.go
+++ main.go
@@ -13,12 +13,17 @@ var isGUI bool
 func init() {
 	fmt.Println("Seagull start")
 	flag.BoolVar(&Testing, "test", false, "")
+	flag.BoolVar(&isGUI, "gui", false, "")
 	flag.Parse()
 
 	os.Setenv("XDG_SESSION_DESKTOP", "Seagull")
 }
 
 func main() {
+	if isGUI {
+		GUI.NewGtkWindow()
+		return
+	}
 	w, err := NewWindowManager("")
 	if err != nil {
 		panic(err)
